0d4f624fe870bb7e1d7b031297be82a12bd7fe03
ajelesin <ajelesin>
2019-12-03 19:39:42 +0500

    day 3

---
+++ Day01/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Day01
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
+
+        public static void F2()
+        {
+            var total = 0;
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+
+
+            foreach (var line in lines)
+            {
+                var m = int.Parse(line);
+                var f = FuelSum(m);
+                total += f;
+            }
+
+            Console.WriteLine(total);
+        }
+
+        public static int FuelSum(int moduleMass)
+        {
+            var total = 0;
+            var v = moduleMass;
+
+            while ((v = v / 3 - 2) > 0)
+            {
+                total += v;
+            }
+
+            return total;
+        }
+
+        public static void F1()
+        {
+            var total = 0;
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+
+
+            foreach (var line in lines)
+            {
+                var m = int.Parse(line);
+                var f = m / 3 - 2;
+                total += f;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Day02
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F1();
+        }
+
+        static void F1()
+        {
+            var origOpcodes = File.ReadAllLines("..\\..\\..\\input.txt")[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            var opcodes = new int[origOpcodes.Length];
+
+            for (var noun = 0; noun <= 99; noun += 1)
+            {
+                for (var verb = 0; verb <= 99; verb += 1)
+                {
+                    Array.Copy(origOpcodes, 0, opcodes, 0, origOpcodes.Length);
+
+                    opcodes[1] = noun;
+                    opcodes[2] = verb;
+
+
+                    for (var i = 0; i < opcodes.Length; i += 4)
+                    {
+                        if (opcodes[i] == 1)
+                        {
+                            opcodes[opcodes[i + 3]] = opcodes[opcodes[i + 1]] + opcodes[opcodes[i + 2]];
+                        }
+                        else if (opcodes[i] == 2)
+                        {
+                            opcodes[opcodes[i + 3]] = opcodes[opcodes[i + 1]] * opcodes[opcodes[i + 2]];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (opcodes[0] == 19690720)
+                    {
+                        Console.WriteLine($"{noun}; {verb}; {100 * noun + verb}");
+                    }
+                }
+            }
+        }
+    }
+}
