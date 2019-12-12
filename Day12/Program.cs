ea5a2e968b5f3949fb039819729f176bf60045bf
me <me@me>
2019-12-13 00:58:15 +0500

    +++

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -18,14 +18,6 @@ namespace Day12
 
         static void F1()
         {
-            var moons = File.ReadAllLines("..\\..\\..\\input.txt")
-                .Select(o => o.Trim('>'))
-                .Select(o => o.Split(','))
-                .Select(o => new[] { o[0].Split('=')[1], o[1].Split('=')[1], o[2].Split('=')[1] })
-                .Select(o => new[] { new[] { int.Parse(o[0]), int.Parse(o[1]), int.Parse(o[2]) }, new[] { 0, 0, 0 } })
-                .ToList();
-
-            // position, velocity
             var orig = File.ReadAllLines("..\\..\\..\\input.txt")
                 .Select(o => o.Trim('>'))
                 .Select(o => o.Split(','))
@@ -33,78 +25,92 @@ namespace Day12
                 .Select(o => new[] { new[] { int.Parse(o[0]), int.Parse(o[1]), int.Parse(o[2]) }, new[] { 0, 0, 0 } })
                 .ToList();
 
-            long time = 0;
-            while (true)
+            // position, velocity
+
+            var times = new long[3];
+            
+            for (var t = 0; t < 3; t += 1)
             {
-                for (var i = 0; i < moons.Count; i+= 1)
+                long time = 0;
+                var visited = new HashSet<(int,int,int,int,int,int,int,int)>();
+
+                var moons = orig
+                    .Select(o => new int[2][] { new int[3] { o[0][0], o[0][1], o[0][2] }, new int[3] { o[1][0], o[1][1], o[1][2] } })
+                    .ToList();
+
+                while (true)
                 {
-                    for (var j = i + 1; j < moons.Count; j += 1)
+                    for (var i = 0; i < moons.Count; i += 1)
                     {
-                        var m1 = moons[i];
-                        var m2 = moons[j];
-
-                        for (var coord = 0; coord < 3; coord += 1)
+                        for (var j = i + 1; j < moons.Count; j += 1)
                         {
+                            var m1 = moons[i];
+                            var m2 = moons[j];
 
-                            if (m1[0][coord] > m2[0][coord])
+                            for (var coord = 0; coord < 3; coord += 1)
                             {
-                                m1[1][coord] -= 1;
-                                m2[1][coord] += 1;
-                            }
-                            else if (m1[0][coord] < m2[0][coord])
-                            {
-                                m1[1][coord] += 1;
-                                m2[1][coord] -= 1;
+
+                                if (m1[0][coord] > m2[0][coord])
+                                {
+                                    m1[1][coord] -= 1;
+                                    m2[1][coord] += 1;
+                                }
+                                else if (m1[0][coord] < m2[0][coord])
+                                {
+                                    m1[1][coord] += 1;
+                                    m2[1][coord] -= 1;
+                                }
                             }
                         }
                     }
-                }
 
-                for (var i = 0; i < moons.Count; i += 1)
-                {
-                    var m = moons[i];
-                    for (var coord = 0; coord < 3; coord += 1)
+                    for (var i = 0; i < moons.Count; i += 1)
                     {
-                        m[0][coord] += m[1][coord];
+                        var m = moons[i];
+                        for (var coord = 0; coord < 3; coord += 1)
+                        {
+                            m[0][coord] += m[1][coord];
+                        }
                     }
-                }
 
-                time += 1;
 
-                if (Compare(orig, moons))
-                {
-                    Console.Write($"\rFinished at step: {time:#,##0}");
-                    Console.ReadLine();
-                }
 
-                if (time % 1_000_000 == 0)
-                {
-                    Console.Write($"\rStep: {time:#,##0}                  ");
+                    var s = (
+                        moons[0][0][t], moons[1][0][t], moons[2][0][t], moons[3][0][t],
+                        moons[0][1][t], moons[1][1][t], moons[2][1][t], moons[3][1][t]
+                        );
+
+                    if (visited.Contains(s))
+                    {
+                        times[t] = time;
+                        break;
+                    }
+                    else
+                    {
+                        visited.Add(s);
+                    }
+                    time += 1;
                 }
             }
 
-            var P = moons.Select(o => o[0]).Select(o => o.Sum(o => Math.Abs(o))).ToArray();
-            var K = moons.Select(o => o[1]).Select(o => o.Sum(o => Math.Abs(o))).ToArray();
-            var E = Enumerable.Zip(P, K, (p, k) => p * k).Sum();
+            //var P = moons.Select(o => o[0]).Select(o => o.Sum(o => Math.Abs(o))).ToArray();
+            //var K = moons.Select(o => o[1]).Select(o => o.Sum(o => Math.Abs(o))).ToArray();
+            //var E = Enumerable.Zip(P, K, (p, k) => p * k).Sum();
 
-            Console.WriteLine(E);
+            Console.WriteLine(lcm(lcm(times[0], times[1]), times[2]));
         }
 
-        static bool Compare(List<int[][]> orig, List<int[][]> compared)
-        {
-            for (var i = 0; i < orig.Count; i += 1)
-            {
-                for (var j = 0; j < 2; j += 1)
-                {
-                    for (var k = 0; k < 3; k += 1)
-                    {
-                        if (orig[i][j][k] != compared[i][j][k])
-                            return false;
-                    }
-                }
-            }
 
-            return true;
+
+        static long Gcd(long a, long b)
+        {
+            if (b == 0) return a;
+            else return Gcd(b, a % b);
+        }
+
+        static long lcm(long a, long b)
+        {
+            return Math.Abs(a * b) / Gcd(a, b);
         }
     }
 }
