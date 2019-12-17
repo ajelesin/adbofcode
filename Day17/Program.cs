f045fb5ba78c009e63d4ac6992d6e93f6312214c
ajelesin <ajelesin>
2019-12-17 19:55:22 +0500

    +++

---
+++ Day17/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long i = 0;
+            long[] origOpcodes = new long[100_000];
+
+            foreach (long opcode in File.ReadAllLines("..\\..\\..\\input.txt")[0]
+                .Split(',')
+                .Select(long.Parse))
+            {
+                origOpcodes[i++] = opcode;
+            }
+
+            var opcodes = origOpcodes.Select(o => o).ToArray();
+
+            var input = new Queue<long>();
+            var output = new Queue<long>();
+
+            IntCode(opcodes, 0, 0, input, output);
+
+            var field = new List<List<long>>();
+            var row = new List<long>();
+
+            foreach (var ch in output)
+            {
+                if (ch == 10)
+                {
+                    if (row.Count > 0)
+                    {
+                        field.Add(row);
+                        row = new List<long>();
+                    }
+                }
+                else
+                {
+                    row.Add(ch);
+                }
+            }
+
+            var inter = new HashSet<(long, long)>();
+
+            for (var y = 1; y < field.Count - 1; y += 1)
+            {
+                var r = field[y];
+                for (var x = 1; x < r.Count - 1; x += 1)
+                {
+                    if (field[y][x] == '#'
+                        && field[y-1][x] == '#' && field[y+1][x] == '#'
+                        && field[y][x-1] == '#' && field[y][x+1] == '#')
+                    {
+                        inter.Add((y, x));
+                        Console.WriteLine((y, x));
+                    }
+                }
+            }
+
+            Console.WriteLine(inter.Select(o => o.Item1 * o.Item2).Sum());
+
+            input.Clear();
+            output.Clear();
+
+            opcodes = origOpcodes.Select(o => o).ToArray();
+            opcodes[0] = 2;
+
+            var inputs = new[]{
+                "A,A,B,C,B,C,B,C,B,A\n",
+                "R,6,L,12,R,6\n",
+                "L,12,R,6,L,8,L,12\n",
+                "R,12,L,10,L,10\n",
+                "n\n"
+            };
+
+            long opcodePointer = 0;
+            long relativeBase = 0;
+            foreach (var newInput in inputs)
+            {
+                foreach (var ch in newInput)
+                {
+                    input.Enqueue((long)ch);
+                }
+            }
+
+            (opcodePointer, relativeBase) = IntCode(opcodes, opcodePointer, relativeBase, input, output);
+
+            foreach (var ch in output)
+            {
+                if (ch == 10)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write((char) ch);
+                }
+            }
+
+            Console.WriteLine("total dusts is " + output.Last());
+        }
+
+        static (long opcodePointer, long relativeBase) IntCode(long[] opcodes, long opcodePointer, long relativeBase, Queue<long> input, Queue<long> output)
+        {
+            long op = opcodePointer;
+            var modes = new long[] { 0, 0, 0 };
+
+            while (op < opcodes.Length)
+            {
+                long opcode = opcodes[op] % 100;
+                long joinedModes = opcodes[op] / 100;
+
+                if (opcode == 99)
+                {
+                    return (-1, relativeBase);
+                }
+
+                var i = 0;
+                modes[0] = modes[1] = modes[2] = 0;
+                while (joinedModes > 0)
+                {
+                    modes[i++] = joinedModes % 10;
+                    joinedModes /= 10;
+                }
+
+
+                if (opcode == 1)
+                {
+                    // 0 position mode
+                    // 1 immediate mode
+                    // 2 relative mode
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] + opcodes[i2];
+                    op += 4;
+                }
+                else if (opcode == 2)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] * opcodes[i2];
+                    op += 4;
+                }
+                else if (opcode == 3)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    if (input.Count == 0)
+                    {
+                        return (op, relativeBase);
+                    }
+
+                    opcodes[i1] = input.Dequeue();
+                    op += 2;
+                }
+                else if (opcode == 4)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    output.Enqueue(opcodes[i1]);
+                    op += 2;
+                }
+                else if (opcode == 5)
+                {
+                    long i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    long i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+
+                    op = opcodes[i1] != 0 ? opcodes[i2] : op + 3;
+                }
+                else if (opcode == 6)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+
+                    op = opcodes[i1] == 0 ? opcodes[i2] : op + 3;
+                }
+                else if (opcode == 7)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] < opcodes[i2] ? 1 : 0;
+                    op += 4;
+                }
+                else if (opcode == 8)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] == opcodes[i2] ? 1 : 0;
+                    op += 4;
+                }
+                else if (opcode == 9)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    relativeBase += opcodes[i1];
+                    op += 2;
+                }
+                else
+                {
+                    throw new Exception("wrong opcode");
+                }
+            }
+
+            return (-1, relativeBase);
+        }
+    }
+}
