a3f5c89746700879e53399ddc9606538b80df609
me <me@me>
2019-12-20 00:47:07 +0500

    +++

---
+++ Day19/Program.cs
@@ -0,0 +1,193 @@
+﻿namespace Day19
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main() => Part2();
+
+        static void Part2()
+        {
+            long i = 0;
+            long[] origOpcodes = new long[100_000];
+
+            foreach (long opcode in File.ReadAllLines("..\\..\\..\\input.txt")[0]
+                .Split(',')
+                .Select(long.Parse))
+            {
+                origOpcodes[i++] = opcode;
+            }
+
+            long x = 0;
+            long y = 0;
+
+            while (!Check(x + 99, y, origOpcodes))
+            {
+                y += 1;
+                if (!Check(x, y + 99, origOpcodes))
+                    x += 1;
+            }
+
+            Console.WriteLine(x * 10000 + y);
+        }
+
+        static void Part1()
+        {
+            long i = 0;
+            long[] origOpcodes = new long[100_000];
+
+            foreach (long opcode in File.ReadAllLines("..\\..\\..\\input.txt")[0]
+                .Split(',')
+                .Select(long.Parse))
+            {
+                origOpcodes[i++] = opcode;
+            }
+
+            var input = new Queue<long>();
+            var output = new Queue<long>();
+
+            long affected = 0;
+
+            for (var x = 0; x < 50; x += 1)
+            {
+                for (var y = 0; y < 50; y += 1)
+                {
+                    if (Check(x, y, origOpcodes))
+                        affected += 1;
+                }
+            }
+
+            Console.WriteLine($"{affected} points are affected by the tractor beam");
+        }
+
+        static bool Check(long x, long y, long[] origOpcodes)
+        {
+            var input = new Queue<long>();
+            var output = new Queue<long>();
+
+            input.Enqueue(x);
+            input.Enqueue(y);
+
+            var opcodes = origOpcodes.Select(o => o).ToArray();
+            IntCode(opcodes, 0, 0, input, output);
+
+            var droneState = output.Dequeue();
+            return droneState == 1;
+        }
+
+        static (long opcodePointer, long relativeBase) IntCode(long[] opcodes, long opcodePointer, long relativeBase, Queue<long> input, Queue<long> output)
+        {
+            long op = opcodePointer;
+            var modes = new long[] { 0, 0, 0 };
+
+            while (op < opcodes.Length)
+            {
+                long opcode = opcodes[op] % 100;
+                long joinedModes = opcodes[op] / 100;
+
+                if (opcode == 99)
+                {
+                    return (-1, relativeBase);
+                }
+
+                var i = 0;
+                modes[0] = modes[1] = modes[2] = 0;
+                while (joinedModes > 0)
+                {
+                    modes[i++] = joinedModes % 10;
+                    joinedModes /= 10;
+                }
+
+
+                if (opcode == 1)
+                {
+                    // 0 position mode
+                    // 1 immediate mode
+                    // 2 relative mode
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] + opcodes[i2];
+                    op += 4;
+                }
+                else if (opcode == 2)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] * opcodes[i2];
+                    op += 4;
+                }
+                else if (opcode == 3)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    if (input.Count == 0)
+                    {
+                        return (op, relativeBase);
+                    }
+
+                    opcodes[i1] = input.Dequeue();
+                    op += 2;
+                }
+                else if (opcode == 4)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    output.Enqueue(opcodes[i1]);
+                    op += 2;
+                }
+                else if (opcode == 5)
+                {
+                    long i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    long i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+
+                    op = opcodes[i1] != 0 ? opcodes[i2] : op + 3;
+                }
+                else if (opcode == 6)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+
+                    op = opcodes[i1] == 0 ? opcodes[i2] : op + 3;
+                }
+                else if (opcode == 7)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] < opcodes[i2] ? 1 : 0;
+                    op += 4;
+                }
+                else if (opcode == 8)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+                    var i2 = modes[1] == 0 ? opcodes[op + 2] : modes[1] == 1 ? op + 2 : relativeBase + opcodes[op + 2];
+                    var i3 = modes[2] == 0 ? opcodes[op + 3] : modes[2] == 1 ? op + 3 : relativeBase + opcodes[op + 3];
+
+                    opcodes[i3] = opcodes[i1] == opcodes[i2] ? 1 : 0;
+                    op += 4;
+                }
+                else if (opcode == 9)
+                {
+                    var i1 = modes[0] == 0 ? opcodes[op + 1] : modes[0] == 1 ? op + 1 : relativeBase + opcodes[op + 1];
+
+                    relativeBase += opcodes[i1];
+                    op += 2;
+                }
+                else
+                {
+                    throw new Exception("wrong opcode");
+                }
+            }
+
+            return (-1, relativeBase);
+        }
+    }
+}
---
+++ Template/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day19
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main() => Part1();
+
+        static void Part1()
+        {
+            var data = File.ReadAllLines("..\\..\\..\\input.txt");
+            Console.WriteLine(data);
+        }
+    }
+}
